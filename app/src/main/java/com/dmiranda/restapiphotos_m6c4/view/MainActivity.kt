55b62814bb1cf41c40c2519d158e999531f464f9
Daniela Miranda <128627388+SrWoonejos@users.noreply.github.com>
2023-10-04 16:36:19 -0300

    avance2

--- app/src/main/java/com/dmiranda/restapiphotos_m6c4/view/MainActivity.kt
+++ app/src/main/java/com/dmiranda/restapiphotos_m6c4/view/MainActivity.kt
@@ -1,4 +1,43 @@
 package com.dmiranda.restapiphotos_m6c4.view
 
-class MainActivity {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        loadApiData()
+    }
+
+    private fun initRecyclerView(photos: ArrayList<Photo>) {
+        val recyclerView = binding.recyclerViewPhotos
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = PhotoAdapter(photos)
+    }
+
+    private fun loadApiData() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val service = RetrofitClient.retrofitInstance()
+            val call: Response<ArrayList<Photo>> = service.getAllPhotos()
+            val photos: ArrayList<Photo>? = call.body()
+
+            runOnUiThread {
+                if (call.isSuccessful) {
+                    if (photos != null) {
+                        initRecyclerView(photos)
+                    }
+                }else
+                {
+                    showToast()
+                }
+            }
+        }
+    }
+
+    private fun showToast(){
+        Toast.makeText(this,"No se pudo cargar los datos desde la API",Toast.LENGTH_SHORT).show()
+    }
 }
